72f7f3c683ff14d3580286f1d3d4f75bf586648c
Asus <Asus@Asus>
2022-12-22 15:28:19 +0300

    Добавлена функция поиска.

--- books/views.py
+++ books/views.py
@@ -1,6 +1,8 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, \
+    PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
 from .models import Book
+from django.db.models import Q
 
 
 class BookListView(LoginRequiredMixin, ListView):
@@ -10,9 +12,21 @@ class BookListView(LoginRequiredMixin, ListView):
     login_url = 'account_login'
 
 
-class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin,  DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
     login_url = 'account_login'
     permission_required = 'books.special_status'
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
